d10ce7982d401abecc2e98ab34de6dbb8d9809e6
superEVILFACE <94980017+superEVILFACE@users.noreply.github.com>
2024-09-08 09:45:05 -0400

    Create GameStatsManager.cc

---
+++ src/GameStatsManager.cc
@@ -0,0 +1,10 @@
+#include <auby.hh>
+
+namespace 
+{
+bool $(GameStatsManager::isIconUnlocked)(void* self, int p1, int p2)
+  return true;
+
+bool $(GameStatsManager::isColorUnlocked)(void* self, int p1, int p2)
+  return true;
+}
