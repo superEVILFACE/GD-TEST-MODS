9537a0b40f3d3ccea69114a1845438bd8a7990e6
NotSynthic <94980017+superEVILFACE@users.noreply.github.com>
2024-09-08 14:20:23 -0400

    gerp

---
+++ src/GameManager.cc
@@ -0,0 +1,16 @@
+#include <auby.hh>
+
+namespace
+{
+
+    bool $(GameManager::isIconUnlocked)(void* self, int p1, int p2)
+    {
+        return true;
+    }
+
+    bool $(GameManager::isColorUnlocked)(void* self, int p1, int p2)
+    {
+        return true;
+    }
+
+}
---
+++ src/MenuLayer.cc
@@ -0,0 +1,16 @@
+#include <auby.hh>
+
+namespace
+{
+
+    bool $(MenuLayer::init)(MenuLayer* self)
+    {
+        $orig(self);
+
+        auto btn = CCSprite::createWithSpriteFrameName("pathIcon_10_001.png");
+        self->addChild(btn);
+
+        return true;
+    }
+    
+}
