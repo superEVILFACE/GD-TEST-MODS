233a4cf7fe40c014029b5727e37a8ea37df3ee72
NotSynthic <94980017+superEVILFACE@users.noreply.github.com>
2024-09-08 19:03:30 -0400

    does this work??

--- src/MenuLayer.cc
+++ src/MenuLayer.cc
@@ -17,7 +17,7 @@ namespace
 
     void* $(MenuLayer::onMoreGames)(MenuLayer* self, CCObject*)
     {
-        ModUtils::createAlert("Not Geode???", "Hello from my custom iOS mod!", "OK");
+        FLAlertLayer::create("Not Geode???", "Hello from my custom iOS mod!", "OK")->show();
     }
 
 }
