5773491c8d4cbb0ea46f8a9c264d7cc807050772
NotSynthic <94980017+superEVILFACE@users.noreply.github.com>
2024-09-08 17:28:38 -0400

    Update MenuLayer.cc

--- src/MenuLayer.cc
+++ src/MenuLayer.cc
@@ -5,7 +5,7 @@ namespace
 
     void* $(MenuLayer::onMoreGames)(MenuLayer* self, CCObject*)
     {
-
+        FLAlertLayer::create("Not Geode???", "Hello from my custom iOS mod!", "OK")->show();
     }
 
 }
